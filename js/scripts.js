5ef540f78271e303d9f7d3d6179225c795b55776
XavierVelasquezSoto <xaviervelasquez_s@hotmail.com>
2024-12-03 01:02:15 +0100

    update

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+//  <div class="container-checkbox">
+//           <input id="test-id" type="checkbox" class="test-checkbox" />
+//           <label for="test-id" class="test-label">test text</label>
+//           <img
+//             src="./assets/img/icon-cross.svg"
+//             alt="cross"
+//             class="cancel-list"
+//           />
+//         </div>
